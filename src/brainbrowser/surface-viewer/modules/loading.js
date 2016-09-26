58d065d2903993adad42a120780e818078934ad0
pandafulmanda <pandafulmanda@gmail.com>
2016-09-26 00:37:59 -0500

    initial parsing options work, exploring loading in multiple intensities for one volume

--- src/brainbrowser/surface-viewer/modules/loading.js
+++ src/brainbrowser/surface-viewer/modules/loading.js
@@ -298,6 +298,11 @@ BrainBrowser.SurfaceViewer.modules.loading = function(viewer) {
 
     loader.loadFromFile(file_input, loadIntensityData, options);
   };
+  viewer.loadIntensityDataSetFromFile = function(file_input, options) {
+    options = checkBinary("intensity_data_types", options);
+
+    loader.loadFromFile(file_input, loadIntensityDataSet, options);
+  };
 
   /**
   * @doc function
@@ -377,9 +382,12 @@ BrainBrowser.SurfaceViewer.modules.loading = function(viewer) {
 
   function loadIntensityData(text, filename, options) {
     options            = options        || {};
-    var name           = options.name   || filename;
     var type           = options.format || "text";
+    var parse_options  = options.parse  || {};
+
+    var name           = options.name   || filename;
     var blend          = options.blend;
+
     var model_name     = options.model_name;
     var model_data     = viewer.model_data.get(model_name);
     var intensity_data = model_data.intensity_data[0];
@@ -395,10 +403,35 @@ BrainBrowser.SurfaceViewer.modules.loading = function(viewer) {
       };
     }
 
-    SurfaceViewer.parseIntensityData(text, type, function(intensity_data) {
-      var min;
-      var max;
+    SurfaceViewer.parseIntensityData(text, type, parse_options, processIntensityData);
+  }
 
+  function loadIntensityDataSet(text, filename, options) {
+    options            = options        || {};
+    var name           = options.name   || filename;
+    var type           = options.format || "text";
+    var blend          = options.blend;
+    var model_name     = options.model_name;
+    var model_data     = viewer.model_data.get(model_name);
+    var intensity_data = model_data.intensity_data[0];
+    var parse_options   = options.parse  || {};
+
+
+    var old_range = {};
+
+    model_name = model_name || model_data.name;
+
+    if (viewer.getAttribute("fix_color_range") && intensity_data) {
+      old_range = {
+        min: intensity_data.range_min,
+        max: intensity_data.range_max
+      };
+    }
+
+    SurfaceViewer.parseIntensityData(text, type, parse_options, processIntensityDataSet);
+  }
+
+<<<<<<< HEAD
       if (intensity_data.colors) {
         loadColorMap(BrainBrowser.createColorMap(intensity_data.colors));
       }
@@ -411,32 +444,49 @@ BrainBrowser.SurfaceViewer.modules.loading = function(viewer) {
         min = options.min === undefined ? intensity_data.min : options.min;
         max = options.max === undefined ? intensity_data.max : options.max;
       }
+=======
+  function processIntensityDataSet(intensity_data_set) {
+    intensity_data_set.forEach(processIntensityData);
+  }
+>>>>>>> initial parsing options work, exploring loading in multiple intensities for one volume
 
-      intensity_data.name = name;
+  function processIntensityData(intensity_data) {
+    var min;
+    var max;
 
-      if (!blend) {
-        model_data.intensity_data.length = 0;
-      }
+    if (viewer.getAttribute("fix_color_range") &&
+        old_range.min !== undefined && old_range.max !== undefined) {
+      min = old_range.min;
+      max = old_range.max;
+    } else {
+      min = options.min === undefined ? intensity_data.min : options.min;
+      max = options.max === undefined ? intensity_data.max : options.max;
+    }
 
-      model_data.intensity_data.push(intensity_data);
-      intensity_data.model_data = model_data;
+    intensity_data.name = name;
 
-      intensity_data.range_min = min;
-      intensity_data.range_max = max;
+    if (!blend) {
+      model_data.intensity_data.length = 0;
+    }
 
-      if (model_data.intensity_data.length > 1) {
-        viewer.blend(options.complete);
-      } else {
-        viewer.updateColors({
-          model_name: model_name,
-          complete: options.complete
-        });
-      }
+    model_data.intensity_data.push(intensity_data);
+    intensity_data.model_data = model_data;
 
-      viewer.triggerEvent("loadintensitydata", {
-        model_data: model_data,
-        intensity_data: intensity_data
+    intensity_data.range_min = min;
+    intensity_data.range_max = max;
+
+    if (model_data.intensity_data.length > 1) {
+      viewer.blend(options.complete);
+    } else {
+      viewer.updateColors({
+        model_name: model_name,
+        complete: options.complete
       });
+    }
+
+    viewer.triggerEvent("loadintensitydata", {
+      model_data: model_data,
+      intensity_data: intensity_data
     });
   }
 
